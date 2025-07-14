ba9fba5ffa142f2c08e7bb4e1eef0c34f2fc67f3
marcoortiztorres <maor9361@colorado.edu>
2025-07-14 12:41:19 -0600

    adding summarySE

--- Practice1.R
+++ Practice1.R
@@ -1,17 +1,27 @@
 install.packages("dplyr")
 library(dplyr)
+library(ggplot2)
+library(Rmisc)
 
-data <- read.csv(file.choose())
+data <- read.csv("Brain_Tumor_Prediction_Dataset.csv")
 names(data)[names(data) == "Survival_Rate..."] <- "Survived"
+is.numeric(data$Survived)
 table(data$Survived)
 head(data)
 
 data$AgeGroup <- cut(data$Age, breaks=seq(0,100,10), right=FALSE,
                      labels=c("0-9","10-19","20-29","30-39","40-49","50-59","60-69","70-79","80-89","90-99"))
 
+t.test(data$Age, data$Survived)
+
+summarySE(data, measurevar = c("Survived"), groupvars = c("AgeGroup", "Country"), na.rm = TRUE)
+
 survival_by_age <- data %>%
   group_by(AgeGroup) %>%
   summarise(Avg_SurvivalRate = mean(`Survived`, na.rm=TRUE))
+
+ggplot(data, aes(x = Age, y = Survived)) +
+  geom_rug(color = "skyblue", size = 3, alpha=.005)
 
 barplot(survival_by_age$Avg_SurvivalRate,
         names.arg = survival_by_age$AgeGroup,
